127d53d3f4b9998037e9bc20ef2f6eac1330fbe1
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-09-30 17:34:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var ServerID = "495677413464997889"; //اي دي السيرفر
-var ChannelID = "495677521346428930";// اي دي الروم
+var ServerID1 = "495677413464997889"; //اي دي السيرفر
+var ServerID2 = "495677413464997889"; //
+var ServerID3 = "495677413464997889"; //
+var ServerID4 = "495677413464997889"; //
+var ServerID5 = "495677413464997889"; //
 
+var ChannelID1 = "495837784804687872";// اي دي الروم
+var ChannelID2 = "495837842010931200";// 
+var ChannelID3 = "495837892875386890";// 
+var ChannelID4 = "495837903730114560";// 
+var ChannelID5 = "495837915524366350";// 
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -20,12 +28,39 @@ client.on('reconnecting', () => console.log('PROBOT credits miner is reconnectin
 
 function timerFunc() {
     client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7))
+        client.guilds.get(ServerID1).channels.get(ChannelID1).send(Math.random().toString(36).substring(7))
 
 
     });
 }
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID2).channels.get(ChannelID2).send(Math.random().toString(36).substring(7))
 
+
+    });
+}
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID3).channels.get(ChannelID3).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID4).channels.get(ChannelID4).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID5).channels.get(ChannelID5).send(Math.random().toString(36).substring(7))
+
+
+    });
+}
 var timer = setTimeout(timerFunc, 1000);
 
 client.login(process.env.BOT_TOKEN);
